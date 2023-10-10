01714ffd426c4cd90d8102568c694713ce43f492
AnastasiyaShved <pav159208@gmail.om>
2023-10-10 19:29:33 +0300

    created project

---
+++ REST_API_Postman_Alamofire/REST_API_Postman_Alamofire/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  REST_API_Postman_Alamofire
+//
+//  Created by Apple on 10.10.23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
