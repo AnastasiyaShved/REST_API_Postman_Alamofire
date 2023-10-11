91881461a02704108890089c99ecd43ccc6ce133
AnastasiyaShved <pav159208@gmail.om>
2023-10-11 18:27:52 +0300

    сделан интерфейс

---
+++ REST_API_Postman_Alamofire/REST_API_Postman_Alamofire/Controllers/ActionCVC.swift
@@ -0,0 +1,13 @@
+//
+//  ActionCVC.swift
+//  REST_API_Postman_Alamofire
+//
+//  Created by Apple on 11.10.23.
+//
+
+import UIKit
+
+class ActionCVC: UICollectionViewCell {
+    
+    @IBOutlet weak var infoLbl: UILabel!
+}
